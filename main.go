f2aa7731659fa03c20ac149b54ad3a5269212445
bookun <kutsuzawa2851@gmail.com>
2018-09-24 01:28:53 +0900

    [feature] lambda connect to firestore

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/kutsuzawa/slim-load-recorder/client"
 )
 
+// Response is format for response from lambda
 type Response struct {
 	Message string `json:"message"`
 }
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	db, err := client.NewDatabase()
+	if err != nil {
+		res := Response{Message: err.Error()}
+		return events.APIGatewayProxyResponse{Body: res.Message, StatusCode: 500}, nil
+	}
+	db.AddUser("test_group", "test_user")
 	res := Response{Message: "hello lambda"}
 	return events.APIGatewayProxyResponse{Body: res.Message, StatusCode: 200}, nil
 }
